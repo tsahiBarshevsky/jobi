9b5aa503871f50418a053b455b7dd9fc669237ee
tsahiBarshevsky <tsahi.13@gmail.com>
2021-07-16 11:11:28 +0300

    Add new job, edit one and edit status

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,86 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 // Connect to database
-mongoose.connect('mongodb://localhost:27017/jobi', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/jobi', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useFindAndModify: false 
+});
+
+// Add new job
+app.post('/add-new-job', async (req, res) =>
+{
+    const newJob = new JobModel({
+        owner: req.body.owner,
+        title: req.body.title,
+        company: req.body.company,
+        status: 'Applied',
+        date: req.body.date,
+        archived: false
+    });
+    await newJob.save();
+    console.log(`${req.body.title} added successfully`);
+    res.json(`${req.body.title} added successfully`);
+});
+
+// Edit job
+app.post('/edit-job', async (req, res) =>
+{
+    var id = req.query.id;
+    var title = req.body.title;
+    var company = req.body.company;
+    var status = req.body.status;
+    var date = req.body.date;
+    JobModel.findByIdAndUpdate(id, 
+        {
+            title: title,
+            company: company,
+            status: status,
+            date: date
+        },
+        function(err)
+        {
+            if (err)
+            {
+                console.log(err);
+                res.status(500).send(err);
+            }
+            else
+            {
+                console.log(`${title} edited successfully`);
+                res.json(`${title} edited successfully`);
+            }
+        }
+    );
+});
+
+// Update job status
+app.post('/update-job-status', async (req, res) =>
+{
+    var id = req.query.id;
+    var status = req.body.status;
+    JobModel.findByIdAndUpdate(id, { status: status },
+        function(err)
+        {
+            if (err)
+            {
+                console.log(err);
+                res.status(500).send(err);
+            }
+            else
+            {
+                console.log(`Status has changed to ${status}`);
+                res.json(`Status has changed to ${status}`);
+            }
+        }
+    );
+});
 
 // Get single job
 app.get('/get-single-job', async (req, res) => 
 {
-    var title = req.query.title;
-    JobModel.findOne({"title": `${title}`}, (err, result) =>
+    var id = req.query.id;
+    JobModel.findById(id, (err, result) =>
     {
         if (err)
         {
@@ -29,12 +102,12 @@ app.get('/get-single-job', async (req, res) =>
         {
             if (!result)
             {
-                console.log(`Couldn't find ${title}`);
+                console.log(`Couldn't find a job with id ${id}`);
                 res.json({});
             }
             else
             {
-                console.log(`${title} has found`);
+                console.log(`The job associated with this id is ${result.title}`);
                 res.json(result);
             }
         }
