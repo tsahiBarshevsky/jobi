ab758f98452223467f5455c811de8fd2316614f8
tsahiBarshevsky <tsahi.13@gmail.com>
2021-07-16 17:52:33 +0300

    Basic dnd for start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
 import './App.sass';
 
 const App = () => 
 {
     return (
-        <div>
-            Homepage
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Dashboard} />
+            </Switch>
+        </Router>
     )
 }
 
