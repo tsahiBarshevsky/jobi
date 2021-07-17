2c15aa778bc02d81b8170688d1c29b29689d4086
tsahiBarshevsky <tsahi.13@gmail.com>
2021-07-17 21:51:17 +0300

    DND basic example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard/Dashboard';
+// import Dashboard from './components/Dashboard/Dashboard';
+import Example from './components/Example/Example';
 import './App.sass';
 
 const App = () => 
@@ -8,7 +9,7 @@ const App = () =>
     return (
         <Router>
             <Switch>
-                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/" component={Example} />
             </Switch>
         </Router>
     )
