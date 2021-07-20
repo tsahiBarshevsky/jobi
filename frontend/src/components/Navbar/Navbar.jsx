0c93559b77f1b102feef4de6074fe13e10d4eee2
tsahiBarshevsky <tsahi.13@gmail.com>
2021-07-20 16:33:46 +0300

    Navbar final design and adjustments

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { IconButton, Typography, SwipeableDrawer } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { IconButton, Typography, SwipeableDrawer, Divider, Avatar } from '@material-ui/core';
 import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
 import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 import { MdDashboard, IoStatsChart, IoArchiveSharp, FiLogOut } from 'react-icons/all';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import useStyles from './styles';
 import './Navbar.sass';
 
@@ -11,6 +12,7 @@ const Navbar = ({ user, logout }) =>
 {
     const [openDrawer, setOpenDrawer] = useState(false);
     const classes = useStyles();
+    const location = useLocation();
 
     const handleOpen = () => 
     {
@@ -26,7 +28,7 @@ const Navbar = ({ user, logout }) =>
         <>
             <div className="navbar-container">
                 <div className="menu">
-                    <IconButton className={classes.button} size="small" onClick={handleOpen}><MenuRoundedIcon className="icon"/></IconButton>
+                    <IconButton className={classes.button} onClick={handleOpen}><MenuRoundedIcon className="icon"/></IconButton>
                     <Typography className={classes.text} variant="subtitle1">Menu</Typography>
                 </div>
                 <Link to='/'>
@@ -47,33 +49,61 @@ const Navbar = ({ user, logout }) =>
                             size="small" 
                             disableRipple onClick={handleClose} 
                             className={classes.close}
-                            >
-                                <CloseRoundedIcon />
+                        >
+                            <CloseRoundedIcon />
                         </IconButton>
                     </div>
                     {user && 
                     <div className="user-info">
-                        <img className="image" src={user.photoURL} alt="pic" />
-                        <Typography className={classes.text} variant="h6">{user.email}</Typography>
+                        {user.photoURL ? 
+                        <img className="image" src={user.photoURL} alt={user.email} />
+                        :
+                        <Avatar className={classes.avatar}>
+                            <Typography className={classes.text} variant="h3">
+                                {user.email.charAt(0).toUpperCase()}
+                            </Typography>
+                        </Avatar>}
+                        {user.displayName ?
+                        <Typography className={classes.text} variant="h6">{user.displayName}</Typography>
+                        :
+                        <Typography className={classes.text} variant="h6">{user.email}</Typography>}
                     </div>}
                     <ul className="options">
                         <li className="option" onClick={handleClose}>
                             <MdDashboard className="icon" />
-                            <Typography className={classes.menuItem} variant="h6">Dashboard</Typography>
+                            {location.pathname !== '/dashboard' ?
+                            <Link className={classes.link} to='/dashboard'>
+                                <Typography className={classes.menuItem} variant="h6">Dashboard</Typography>
+                            </Link>
+                            :
+                            <Typography className={classes.menuItem} variant="h6">Dashboard</Typography>}
                         </li>
                         <li className="option" onClick={handleClose}>
                             <IoStatsChart className="icon" />
-                            <Typography className={classes.menuItem} variant="h6">Statistics</Typography>
+                            {location.pathname !== '/statistics' ?
+                            <Link className={classes.link} to='/statistics'>
+                                <Typography className={classes.menuItem} variant="h6">Statistics</Typography>
+                            </Link>
+                            :
+                            <Typography className={classes.menuItem} variant="h6">Statistics</Typography>}
                         </li>
                         <li className="option" onClick={handleClose}>
                             <IoArchiveSharp className="icon" />
-                            <Typography className={classes.menuItem} variant="h6">Archive</Typography>
-                        </li>
-                        <li className="option" onClick={logout}>
-                            <FiLogOut className="icon logout" />
-                            <Typography className={classes.menuItem} variant="h6">Logout</Typography>
+                            {location.pathname !== '/archive' ?
+                            <Link className={classes.link} to='/archive'>
+                                <Typography className={classes.menuItem} variant="h6">Archive</Typography>
+                            </Link>
+                            :
+                            <Typography className={classes.menuItem} variant="h6">Archive</Typography>}
                         </li>
                     </ul>
+                    <div className="logout-container">
+                        <Divider className={classes.divider} />
+                        <div className="logout" onClick={logout}>
+                            <FiLogOut className="icon" />
+                            <Typography className={classes.menuItem} variant="h6">Logout</Typography>
+                        </div>
+                    </div>
                 </div>
             </SwipeableDrawer>
         </>
--- frontend/src/components/Navbar/styles.js
+++ frontend/src/components/Navbar/styles.js
@@ -3,8 +3,32 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
     text: { fontFamily: `'Poppins', sans-serif`, letterSpacing: 1 },
     menuItem: { fontFamily: `'Poppins', sans-serif`, letterSpacing: 2 },
-    button: { marginRight: 10, marginLeft: -10 },
-    close: { color: 'white'},
+    close: { color: 'white' },
+    avatar:
+    {
+        width: 90,
+        height: 90,
+        marginBottom: 10
+    },
+    divider: 
+    {
+        width: '100%', 
+        backgroundColor: '#ffffff1A',
+        borderRadius: 25,
+        marginBottom: 5
+    },
+    button: 
+    { 
+        marginRight: 10, 
+        marginLeft: -10, 
+        width: 40,
+        height: 40,
+    },
+    link:
+    {
+        textDecoration: 'none',
+        color: 'white'
+    },
     paper:
     {
         width: 350,
